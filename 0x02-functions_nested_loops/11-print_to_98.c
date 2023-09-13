6547c929b0ec87236c8dd26c84850f66a279210f
okotoazachristian <okotoazachristian@gmail.com>
2023-09-13 06:28:24 -0700

    a function that prints all natural numbers from n to 98, followed by a new line

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -46,22 +46,3 @@ void print_to_98(int n)
 	}
 	}
 }
-
-
-11-main.c
-#include "main.h"
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_to_98(0);
-	print_to_98(98);
-	print_to_98(111);
-	print_to_98(81);
-	print_to_98(-10);
-	return (0);
-}
-
